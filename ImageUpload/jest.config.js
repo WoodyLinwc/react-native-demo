9ef7107f9c5b2ad638c3cf8596b173f336684db0
Woody Lin <wancheng.lin001@gmail.com>
2025-09-03 23:47:08 -0400

    Update jest.config.js

--- ImageUpload/jest.config.js
+++ ImageUpload/jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
     '\\.(css)$': 'identity-obj-proxy',
   },
   transformIgnorePatterns: [
-    'node_modules/(?!(react-native|@react-native|react-native-reanimated|react-native-safe-area-context|nativewind)/)',
+    'node_modules/(?!(react-native|@react-native|react-native-reanimated|react-native-safe-area-context|nativewind|@react-navigation|react-native-gesture-handler|react-native-screens|react-native-vector-icons|react-native-worklets)/)',
   ],
+  setupFilesAfterEnv: ['<rootDir>/jest-setup.js'],
 };

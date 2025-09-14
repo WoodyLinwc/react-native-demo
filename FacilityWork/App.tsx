128fdf1cd9c79eeb6735a2fc8caf166262878395
Woody Lin <wancheng.lin001@gmail.com>
2025-09-13 22:22:14 -0400

    Update App.tsx

--- FacilityWork/App.tsx
+++ FacilityWork/App.tsx
@@ -9,11 +9,30 @@ import React from 'react';
 import 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 
 import HomeScreen from './screens/HomeScreen';
 
+import TaskScreen from './screens/tabs/TaskScreen';
+import InventoryScreen from './screens/tabs/InventoryScreen';
+// import HistoryScreen from './screens/tabs/HistoryScreen';
+import NotificationScreen from './screens/tabs/NotificationScreen';
+
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function TabNavigator() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Tasks" component={TaskScreen} />
+      <Tab.Screen name="Inventory" component={InventoryScreen} />
+      {/* <Tab.Screen name="History" component={HistoryScreen} /> */}
+      <Tab.Screen name="Notifications" component={NotificationScreen} />
+    </Tab.Navigator>
+  );
+}
 
 function App() {
   return (
@@ -21,8 +40,8 @@ function App() {
       <NavigationContainer>
         <Stack.Navigator>
           <Stack.Screen
-            name="Home"
-            component={HomeScreen}
+            name="Main"
+            component={TabNavigator}
             options={{ headerShown: false }}
           />
         </Stack.Navigator>

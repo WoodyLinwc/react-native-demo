7c1c7afa57d586bd031855ef11095c3352edd17f
Woody Lin <wancheng.lin001@gmail.com>
2025-09-13 22:14:32 -0400

    Update App.tsx

--- FacilityWork/App.tsx
+++ FacilityWork/App.tsx
@@ -10,57 +10,16 @@ import 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import {
-  RootStackParamList,
-  DrawerParamList,
-  TabParamList,
-} from './types/navigation';
 
-// drawer
-import SettingsScreen from './screens/drawer/SettingsScreen';
-// tabs
-import HistoryScreen from './screens/tabs/HistoryScreen';
-import InventoryScreen from './screens/tabs/InventoryScreen';
-import NotificationScreen from './screens/tabs/NotificationScreen';
-import TaskScreen from './screens/tabs/TaskScreen';
 import HomeScreen from './screens/HomeScreen';
 
 const Stack = createNativeStackNavigator();
-const Drawer = createDrawerNavigator();
-const Tab = createBottomTabNavigator();
-
-function TabNavigator() {
-  return (
-    <Tab.Navigator>
-      <Tab.Screen name="Tasks" component={TaskScreen} />
-      <Tab.Screen name="Inventory" component={InventoryScreen} />
-      <Tab.Screen name="History" component={HistoryScreen} />
-      <Tab.Screen name="Notifications" component={NotificationScreen} />
-    </Tab.Navigator>
-  );
-}
-
-function DrawerNavigator() {
-  return (
-    <Drawer.Navigator>
-      <Drawer.Screen name="Main" component={TabNavigator} />
-      <Drawer.Screen name="Settings" component={SettingsScreen} />
-    </Drawer.Navigator>
-  );
-}
 
 function App() {
   return (
     <SafeAreaProvider>
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen
-            name="Dashboard"
-            component={DrawerNavigator}
-            options={{ headerShown: false }}
-          />
           <Stack.Screen
             name="Home"
             component={HomeScreen}

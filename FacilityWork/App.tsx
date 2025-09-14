5b88518b53419bab42ca0489329d10d4500ddfac
Woody Lin <wancheng.lin001@gmail.com>
2025-09-13 21:33:10 -0400

    navigation

--- FacilityWork/App.tsx
+++ FacilityWork/App.tsx
@@ -8,17 +8,58 @@
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+//
 import HomeScreen from './screens/HomeScreen';
+// drawer
+import SettingsScreen from './screens/drawer/SettingsScreen';
+// tabs
+import HistoryScreen from './screens/tabs/HistoryScreen';
+import InventoryScreen from './screens/tabs/InventoryScreen';
+import NotificationScreen from './screens/tabs/NotificationScreen';
+import TaskScreen from './screens/tabs/TaskScreen';
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+const Tab = createBottomTabNavigator();
+
+function TabNavigator() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Tasks" component={TaskScreen} />
+      <Tab.Screen name="Inventory" component={InventoryScreen} />
+      <Tab.Screen name="History" component={HistoryScreen} />
+      <Tab.Screen name="Notifications" component={NotificationScreen} />
+    </Tab.Navigator>
+  );
+}
+
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen name="Main" component={TabNavigator} />
+      <Drawer.Screen name="Settings" component={SettingsScreen} />
+    </Drawer.Navigator>
+  );
+}
 
 function App() {
   return (
     <SafeAreaProvider>
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Dashboard"
+            component={DrawerNavigator}
+            options={{ headerShown: false }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>

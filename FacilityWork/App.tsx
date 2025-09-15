68c96a23830031260fab06cac78ed9a4670a3c36
Woody Lin <wancheng.lin001@gmail.com>
2025-09-15 16:30:25 -0400

    drawer navigation

--- FacilityWork/App.tsx
+++ FacilityWork/App.tsx
@@ -11,6 +11,7 @@ import './global.css';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 
 import HomeScreen from './screens/HomeScreen';
@@ -18,17 +19,48 @@ import HomeScreen from './screens/HomeScreen';
 import TaskScreen from './screens/tabs/TaskScreen';
 import InventoryScreen from './screens/tabs/InventoryScreen';
 import NotificationScreen from './screens/tabs/NotificationScreen';
+import SettingsScreen from './screens/drawer/SettingsScreen';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
+
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen
+        name="Dashboard"
+        component={TabNavigator}
+        options={{ title: 'Dashboard', headerShown: true }}
+      />
+      <Drawer.Screen name="Setting" component={SettingsScreen} />
+    </Drawer.Navigator>
+  );
+}
 
 function TabNavigator() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Tasks" component={TaskScreen} />
-      <Tab.Screen name="Inventory" component={InventoryScreen} />
-      <Tab.Screen name="Notifications" component={NotificationScreen} />
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{ headerShown: false }}
+      />
+      <Tab.Screen
+        name="Tasks"
+        component={TaskScreen}
+        options={{ headerShown: false }}
+      />
+      <Tab.Screen
+        name="Inventory"
+        component={InventoryScreen}
+        options={{ headerShown: false }}
+      />
+      <Tab.Screen
+        name="Notifications"
+        component={NotificationScreen}
+        options={{ headerShown: false }}
+      />
     </Tab.Navigator>
   );
 }
@@ -40,7 +72,7 @@ function App() {
         <Stack.Navigator>
           <Stack.Screen
             name="Main"
-            component={TabNavigator}
+            component={DrawerNavigator}
             options={{ headerShown: false }}
           />
         </Stack.Navigator>
